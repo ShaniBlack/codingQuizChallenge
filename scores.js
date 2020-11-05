027f38b6648e56549eb5e905309a86c3128982a6
Shani Black <shaniblack2004@gmail.com>
2020-11-04 23:11:38 -0600

    created scores.js/added function to show high scores and to clear high scores from local storage

---
+++ scores.js
@@ -0,0 +1,19 @@
+function showHighScores() {
+    let highScores = JSON.parse(localstorage.getItem("highscores")) || [];
+
+    highScores.forEach(function (score) {
+        let liScore = document.createElement("li")
+        liScore.textContent = score.name + " - " + score.score;
+
+        let olScore = document.getElementById("highScores");
+        olScore.appendChild(liScore);
+    });
+}
+function clearScores() {
+    localStorage.removeItem("highScores");
+    location.reload();
+}
+
+document.getElementById("clear").onclick = clearScores;
+
+showHighScores();
