aac3ef3d81145dfbab9053ff95504bf5aab080d4
Shani Black <shaniblack2004@gmail.com>
2020-11-04 22:24:41 -0600

    added score html and linked to index.html/created the Highscores page, including Go Back button and Clear Highscores button

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let submitButton = document.querySelector("#submit");
 let startButton = document.querySelector("#start");
 let scoreName = document.querySelector("#name");
 let responses = document.querySelector("#response");
-let asksQues = document.querySelector("asks")
+let asksQues = document.querySelector("asks");
 let answerQues = document.querySelector("answer");
 
 let questionNumber = 0;
@@ -13,26 +13,54 @@ let time = 75;
 let timeClock;
 
 function startQuiz() {
-    let startPageEl = document.getElementById("startPage");
-    startPageEl.setAttribute("class", "hide");
+  let startPageEl = document.getElementById("startPage");
+  startPageEl.setAttribute("class", "hide");
 
-    questionsSection.removeAttribute("class");
+  questionsSection.removeAttribute("class");
 
-    timeClock = setInterval(clockCounter, 1000);
+  timeClock = setInterval(clockCounter, 1000);
 
-    timer.textContent = time;
+  timer.textContent = time;
 
+  getQuestions();
 }
 
 startButton.onclick = startQuiz;
 
-function getQuestions() {
-    questionNumber++;
-    answer = asks[questionNumber].answer;
+function getQuestion() {
+  let currentQuestion = asks[currentQuestionIndex];
+  let asksEl = document.getElementById("asksQuestion");
+  asksEl.textContent = currentQuestion.ask;
 
-    asksQues.textContent = asks[questionNumber].ask;
-    answerQues.innerHTML = "";
+  choicesDiv.innerHTML = "";
+  currentQuestion.choices.forEach(function (choice, i) {
+    let choicePick = document.createElement("button");
+    choicePick.setAttribute("class", "choice");
+    choicePick.setAttribute("value", choice);
 
-    let choices = asks[questionNumber].choices;
+    choicePick.textContent = i + 1 + ". " + choice;
+    choicePick.onclick = questionClick;
+    choicesDiv.appendChild(choicePick);
+  });
+}
 
-      }
+function questionClick() {
+  if (this.value !== asks[currentQuestionIndex].answer) {
+    time -= 10;
+
+    if (time < 0) {
+      time = 0;
+    }
+    timer.textContent = time;
+    response.textContent = "Sorry! Wrong answer!";
+  } else {
+    response.textContent = "Yay! You got it!";
+  }
+
+  currentQuestionIndex++;
+  if (currentQuestionIndex === questions.length) {
+    quizEnd();
+  } else {
+    getQuestion();
+  }
+}
