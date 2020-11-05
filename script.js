5e7eca5109960ad2a74bd068cbcf2d2ac7f3ab76
Shani Black <shaniblack2004@gmail.com>
2020-11-04 21:54:49 -0600

    attempted to start Quiz and time clock and initialize questions. not currently working

--- script.js
+++ script.js
@@ -1,16 +1,38 @@
-let questionsAsked = document.querySelector("#questions");
+let questionsSection = document.querySelector("#questions");
 let timer = document.querySelector("#time");
-let choicesGiven = document.querySelector("#choices");
+let choicesDiv = document.querySelector("#choices");
 let submitButton = document.querySelector("#submit");
 let startButton = document.querySelector("#start");
 let scoreName = document.querySelector("#name");
 let responses = document.querySelector("#response");
+let asksQues = document.querySelector("asks")
+let answerQues = document.querySelector("answer");
+
+let questionNumber = 0;
+let time = 75;
+let timeClock;
 
 function startQuiz() {
     let startPageEl = document.getElementById("startPage");
     startPageEl.setAttribute("class", "hide");
 
-    questionsAsked.removeAttribute("class");
+    questionsSection.removeAttribute("class");
 
-    
+    timeClock = setInterval(clockCounter, 1000);
+
+    timer.textContent = time;
+
 }
+
+startButton.onclick = startQuiz;
+
+function getQuestions() {
+    questionNumber++;
+    answer = asks[questionNumber].answer;
+
+    asksQues.textContent = asks[questionNumber].ask;
+    answerQues.innerHTML = "";
+
+    let choices = asks[questionNumber].choices;
+
+      }
