ee97087f90474a4f9dff9b5c62d55647cf5bedb7
Shani Black <shaniblack2004@gmail.com>
2020-11-04 18:57:54 -0600

    script.js-set variables for questions,time,choices,submit,start,name,response

---
+++ script.js
@@ -0,0 +1,7 @@
+let questionsAsked = document.querySelector("#questions");
+let timer = document.querySelector("#time");
+let choicesGiven = document.querySelector("#choices");
+let submitButton = document.querySelector("#submit");
+let startButton = document.querySelector("#start");
+let scoreName = document.querySelector("#name");
+let responses = document.querySelector("#response");
